af6f60eadd64f7db7d4117a4645ff5ba61ec279d
IvanVnucec <vnucec.ivan@gmail.com>
2021-03-01 23:52:29 +0100

    added compiler flags

--- scripts/docker_run_container.sh
+++ scripts/docker_run_container.sh
@@ -7,11 +7,3 @@ readonly HOST_WORKDIR=/app  # defined in Dockerfile
 
 echo "Running Docker container."
 docker run --rm -v "$(pwd):$HOST_WORKDIR" $IMAGE_NAME
-docker_exit_code=$?
-
-if [ $docker_exit_code != 0 ]
-then
-    cat ./builddir/meson-logs/testlog.txt
-fi
-
-exit $docker_exit_code
