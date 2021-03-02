2f3f00b165f9c4c610b59dad335cf972bfdb2aab
IvanVnucec <vnucec.ivan@gmail.com>
2021-03-02 19:38:40 +0100

    add add vector tests

--- test/test_add.c
+++ test/test_add.c
@@ -514,6 +514,88 @@ MU_TEST(test_add_success_A_A_A) {
     mu_assert_double_eq(A.data[8], 18.0);
 }
 
+MU_TEST(test_add_success_C_A_B_rowVect) {
+    MTX_Error_E error;
+
+    float dataA[1][3] = {
+        {1, 4, 7}
+    };
+
+    float dataB[1][3] = {
+        {1, 4, 7}
+    };
+
+    float dataC[1][3];
+
+    MTX_Matrix_S A = {
+        .rows = 1,
+        .cols = 3,
+        .data = (float *)dataA
+    };
+
+    MTX_Matrix_S B = {
+        .rows = 1,
+        .cols = 3,
+        .data = (float *)dataB
+    };
+
+    MTX_Matrix_S C = {
+        .rows = 1,
+        .cols = 3,
+        .data = (float *)dataC
+    };
+
+    MTX_add(&C, &A, &B, &error);
+
+    mu_check(error == MTX_Matrix_ERROR_NONE);
+    mu_assert_double_eq(C.data[0],  2.0);
+    mu_assert_double_eq(C.data[1],  8.0);
+    mu_assert_double_eq(C.data[2],  14.0);
+}
+
+MU_TEST(test_add_success_C_A_B_colVect) {
+    MTX_Error_E error;
+
+    float dataA[3][1] = {
+        {1.0},
+        {4.0},
+        {7.0}
+    };
+
+    float dataB[3][1] = {
+        {1.0},
+        {4.0},
+        {7.0}
+    };
+
+    float dataC[3][1];
+
+    MTX_Matrix_S A = {
+        .rows = 3,
+        .cols = 1,
+        .data = (float *)dataA
+    };
+
+    MTX_Matrix_S B = {
+        .rows = 3,
+        .cols = 1,
+        .data = (float *)dataB
+    };
+
+    MTX_Matrix_S C = {
+        .rows = 3,
+        .cols = 1,
+        .data = (float *)dataC
+    };
+
+    MTX_add(&C, &A, &B, &error);
+
+    mu_check(error == MTX_Matrix_ERROR_NONE);
+    mu_assert_double_eq(C.data[0],  2.0);
+    mu_assert_double_eq(C.data[1],  8.0);
+    mu_assert_double_eq(C.data[2],  14.0);
+}
+
 MU_TEST_SUITE(test_suite) {
     MU_RUN_TEST(test_add_fail_err_null);
 
@@ -532,6 +614,9 @@ MU_TEST_SUITE(test_suite) {
     MU_RUN_TEST(test_add_success_A_A_B);
     MU_RUN_TEST(test_add_success_A_B_A);
     MU_RUN_TEST(test_add_success_A_A_A);
+
+    MU_RUN_TEST(test_add_success_C_A_B_rowVect);
+    MU_RUN_TEST(test_add_success_C_A_B_colVect);
 }
 
 int main(int argc, char *argv[]) {
