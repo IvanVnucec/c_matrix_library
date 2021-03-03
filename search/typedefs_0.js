4cd1ceff0b828bbf9ba031e119db8efd3d4b478c
IvanVnucec <IvanVnucec@users.noreply.github.com>
2021-03-03 22:22:58 +0000

    deploy: c5f78a8a4949ada32e56c501d00ec4c4d7d9a66c

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['mtx_5ferror_5fe_52',['MTX_Error_E',['../matrix__math_8h.html#a336e5554a8511665fbe82939fdb8a75d',1,'matrix_math.h']]],
-  ['mtx_5fmatrix_5fs_53',['MTX_Matrix_S',['../matrix__math_8h.html#a1824d7879503f15fa570471fccf5e663',1,'matrix_math.h']]]
+  ['mtx_5ferror_5fe_53',['MTX_Error_E',['../matrix__math_8h.html#a336e5554a8511665fbe82939fdb8a75d',1,'matrix_math.h']]],
+  ['mtx_5fmatrix_5fs_54',['MTX_Matrix_S',['../matrix__math_8h.html#a1824d7879503f15fa570471fccf5e663',1,'matrix_math.h']]]
 ];
