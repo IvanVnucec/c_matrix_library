2d1fa1e557564c559db74ef7f281271e676f5319
IvanVnucec <vnucec.ivan@gmail.com>
2021-03-06 21:31:40 +0100

    shell script update

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -2,7 +2,7 @@
 
 # Clang-format
 echo "Clang-format code formatting."
-clang-format -i --style=file src/*.c
-clang-format -i --style=file src/*.h
-clang-format -i --style=file test/*.c
-clang-format -i --style=file test/*.h
+clang-format -i --verbose --style=file src/*.c
+clang-format -i --verbose --style=file src/*.h
+clang-format -i --verbose --style=file test/*.c
+clang-format -i --verbose --style=file test/*.h
