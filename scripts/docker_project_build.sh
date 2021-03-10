8acc6c9cfd1be97bf52c693f61790b9abe1a963a
IvanVnucec <vnucec.ivan@gmail.com>
2021-03-10 17:39:43 +0100

    meson w/o rm function for faster builds

--- scripts/docker_project_build.sh
+++ scripts/docker_project_build.sh
@@ -4,10 +4,7 @@
 . ./scripts/constants.env
 
 echo "Compile project."
-docker exec -i $IMAGE_NAME bash -c "rm -rf builddir"
-(($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
-
-docker exec -i $IMAGE_NAME bash -c "meson setup builddir -Db_coverage=true"
+docker exec -i $IMAGE_NAME bash -c "CC=gcc meson builddir -Db_coverage=true"
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
 
 docker exec -i $IMAGE_NAME bash -c "meson compile -C builddir"
