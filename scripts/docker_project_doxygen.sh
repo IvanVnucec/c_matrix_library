3ceb2135d4350513a72a6084a0a833ad0ea3647b
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2021-03-20 18:04:27 +0100

    Update docker_project_doxygen.sh

--- scripts/docker_project_doxygen.sh
+++ scripts/docker_project_doxygen.sh
@@ -4,10 +4,10 @@
 . ./scripts/constants.env
 
 echo "Generate Doxygen documentation."
-docker exec -i $IMAGE_NAME bash -c "mkdir docs"
+docker exec -i $IMAGE_NAME bash -c "mkdir -p docs"
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
 
-docker exec -i $IMAGE_NAME bash -c "mkdir docs/Doxygen"
+docker exec -i $IMAGE_NAME bash -c "mkdir -p docs/Doxygen" ./../Doxyfile
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
 
 docker exec -i $IMAGE_NAME bash -c "doxygen Doxyfile ./../Doxyfile"
