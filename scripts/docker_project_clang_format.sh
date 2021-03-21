91a743843983406e2087d8eb6e69c1ee449af04e
IvanVnucec <vnucec.ivan@gmail.com>
2021-03-21 20:42:55 +0100

    - moved files in different folders - better static library creation

--- scripts/docker_project_clang_format.sh
+++ scripts/docker_project_clang_format.sh
@@ -4,16 +4,11 @@
 . ./scripts/constants.env
 
 echo "Clang-format code formatting."
-docker exec -i $IMAGE_NAME bash -c "clang-format-11 -i --verbose --style=file src/*.c"
+
+docker exec -i $IMAGE_NAME bash -c "clang-format-11 -i --verbose --style=file src/matrix_math/*.c"
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
 
-docker exec -i $IMAGE_NAME bash -c "clang-format-11 -i --verbose --style=file src/*.h"
-(($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
-
-docker exec -i $IMAGE_NAME bash -c "clang-format-11 -i --verbose --style=file test/*.c"
-(($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
-
-docker exec -i $IMAGE_NAME bash -c "clang-format-11 -i --verbose --style=file test/*.h"
+docker exec -i $IMAGE_NAME bash -c "clang-format-11 -i --verbose --style=file src/matrix_math/*.h"
 (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
 
 echo "Done Clang-format code formatting."
---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include "matrix_math.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    MTX_Error_E error;
+
+    float dataA[3][3] = {
+        {1.0f, 2.0f, 3.0f}, 
+        {4.0f, 5.0f, 6.0f}, 
+        {7.0f, 8.0f, 9.0f}};
+
+    float dataB[3][3] = {
+        {1.0f, 2.0f, 3.0f}, 
+        {4.0f, 5.0f, 6.0f}, 
+        {7.0f, 8.0f, 9.0f}};
+
+    float dataC[3][3];
+
+    MTX_Matrix_S A, B, C;
+
+    MTX_init(&A, 3, 3, (float *)dataA, &error);
+    MTX_init(&B, 3, 3, (float *)dataB, &error);
+    MTX_init(&C, 3, 3, (float *)dataC, &error);
+
+    // C = cholesky(A)
+    MTX_cholesky(&C, &A, &error);
+
+    // B = A + B
+    MTX_add(&B, &A, &B, &error);
+
+    // C = A * B
+    MTX_mult(&C, &A, &B, &error);
+
+    // print A matrix if MTX_mult succesfull
+    if (error == MTX_Matrix_ERROR_NONE) {
+        int i, j;
+        for (i = 0; i < C.cols; i++) {
+            for (j = 0; j < C.rows; j++) {
+                printf("%.4f ", C.data[i + j * C.cols]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
